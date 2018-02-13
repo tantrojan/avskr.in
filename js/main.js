95d80275d90d02d169deb65be24935fa7a740fdd
Bhola-B2C <bholajrs@gmail.com>
2018-02-13 18:52:05 +0530

    First and aboutus page with parallax js

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var typed = new Typed(".type", {
+  strings: ['Aavishkar', 'The Cradle of Innovation'],
+  stringsElement: null,
+  loop: true,
+  loopCount: Infinity,
+  smartBackspace: true // Default value
+});
+
+
+// $(function() {
+//   $('a').on('click', function(e) {
+//     e.preventDefault();
+//     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+//   });
+// });
+
+// $('.menu-burger, .menu-items').on('click', function() {
+//   $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+//   $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+// });
+
+// $(document).ready(function () {
+//           function rgt() {
+//               $('#clo').animate({ left: "1500" }, 20000, hider);
+//             }
+//             rgt();
+//             function hider() {
+//             $('#clo').css('left', '-2000px');
+//                 rgt();
+//             }
+//         });
+
+// $('body').scroll(function(){
+//   console.log('Hi');
+// })
